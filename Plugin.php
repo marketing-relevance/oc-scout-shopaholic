76137c3601e9e179b3cf8f6e80386a77ff265f12
Eric Pfeiffer <eric@marketingrelevance.com>
2021-01-15 10:54:44 -0600

    Plugin is now working in OctoberCMS v1.1

--- Plugin.php
+++ Plugin.php
@@ -1,6 +1,9 @@
 <?php namespace MarketingRelevance\ScoutShopaholic;
 
+use App;
+use Config;
 use Event;
+use Illuminate\Foundation\AliasLoader;
 use System\Classes\PluginBase;
 
 class Plugin extends PluginBase
@@ -10,6 +13,7 @@ class Plugin extends PluginBase
 
     public function boot()
     {
+        $this->bootPackages();
         $this->addEventListener();
     }
 
@@ -23,5 +27,45 @@ class Plugin extends PluginBase
         Event::subscribe(Classes\Event\CategoryModelHandler::class);
         Event::subscribe(Classes\Event\ProductModelHandler::class);
         Event::subscribe(Classes\Event\TagModelHandler::class);
+    }
+
+    /**
+     * Boots (configures and registers) any packages found within this plugin's packages.load configuration value
+     *
+     * @see https://luketowers.ca/blog/how-to-use-laravel-packages-in-october-plugins
+     * @author Luke Towers <octobercms@luketowers.ca>
+     */
+    public function bootPackages()
+    {
+        // Get the namespace of the current plugin to use in accessing the Config of the plugin
+        $pluginNamespace = str_replace('\\', '.', strtolower(__NAMESPACE__));
+
+        // Instantiate the AliasLoader for any aliases that will be loaded
+        $aliasLoader = AliasLoader::getInstance();
+
+        // Get the packages to boot
+        $packages = Config::get($pluginNamespace . '::packages');
+
+        // Boot each package
+        foreach ($packages as $name => $options) {
+            // Setup the configuration for the package, pulling from this plugin's config
+            if (!empty($options['config']) && !empty($options['config_namespace'])) {
+                Config::set($options['config_namespace'], $options['config']);
+            }
+
+            // Register any Service Providers for the package
+            if (!empty($options['providers'])) {
+                foreach ($options['providers'] as $provider) {
+                    App::register($provider);
+                }
+            }
+
+            // Register any Aliases for the package
+            if (!empty($options['aliases'])) {
+                foreach ($options['aliases'] as $alias => $path) {
+                    $aliasLoader->alias($alias, $path);
+                }
+            }
+        }
     }
 }
